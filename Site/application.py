eb5037ca705756492724fb602f669d099f334bb5
Ahmed Abukar <ahmedabukar@Ahmeds-MacBook-Pro.local>
2020-07-03 22:47:10 -0400

    Created app/webpage framework and webpage search elements.

---
+++ Site/application.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    team = request.form.get("team")
+    name = request.form.get("name")
+    return render_template("result.html", name=name, team=team )
+
+if __name__  == "__main__":
+    app.run()
+
+
+    
